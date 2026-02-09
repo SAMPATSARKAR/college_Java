12b7953abcca53f3f177b3e9eac4c899bccc12cb
Sampat <sampat@Sampats-MacBook-Air.local>
2026-02-09 16:57:17 +0530

    Array intro

---
+++ Feb/Array/array.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class array{
+    public static void main(String[] args){
+
+        int[] arr = new int[5];
+        System.out.print((float)arr[0]);
+        // for(int i=0;i<5;i++){
+        //     arr[i]=i;
+        // }
+        // for(int j=0;j<5;j++){
+        //     System.out.printf("%d",arr);
+        // }
+    }
+}

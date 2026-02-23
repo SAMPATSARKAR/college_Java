fe154b219e83ea9a39c9f7821fbb064e6b458fe8
SAMPATSARKAR <sampat7123@gmail.com>
2026-02-23 16:52:19 +0530

    Completed Polymorphism

--- Feb/OOPS/MultiInhert.java
+++ Feb/OOPS/MultiInhert.java
@@ -63,8 +63,8 @@ public class MultiInhert{
         // c4.greetChild(); // This line would cause a compilation error because reference variable is of type ChildOne and it doesn't have greetChild method, even though the actual object is of type ChildTwo which has greetChild method
         // constructor of childeOne will be called , becoz of that parent constructor will be called and (then child two constructor will be called as it is the object).
         // Parent p2 = new ChildOne(); //upcasting
-        // ChildOne c5 = (ChildOne)p2; //downcasting--implicit
-        // c5.greetChild(); // This line will work because reference variable is of type ChildOne and it has greetChild method, even though the actual object is of type ChildOne which has greetChild method
+        // ChildOne c5 = (ChildOne)p2; //downcasting--explicit only possible when reference variable is of type parent and actual object is of type child and no implicit downcasting is possible because it can lead to runtime error if reference variable is of type parent and actual object is of type parent then it will throw ClassCastException at runtime
+        // c5.greetChild(); 
         // c5.greetParent(); // This line will work because reference variable is of type ChildOne and it has greetParent method, even though the actual object is of type ChildOne which has greetParent method
         // ChildOne c6 = (ChildOne)p2; //downcasting--explicit
         // c6.greetChild(); // This line will work because reference variable is of type ChildOne and it has greetChild method, even though the actual object is of type ChildOne which has greetChild method
@@ -76,5 +76,5 @@ public class MultiInhert{
         System.out.println(p);// it will print the reference variable of parent class because toString() method of Object class is called which returns the reference variable of the object
         Parent p2 = new Parent();
         System.out.println(p2==p);// it will print false because p2 and p are different objects
-    }
+}
 }
